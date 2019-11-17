a6fda884d5f64a17adf6780c57c4ed3adc31ef93
Pradip Shrestha <wafttechnology@Wafts-MacBook-Pro.local>
2019-11-17 12:08:54 +0545

    new_stage

--- src/App.js
+++ src/App.js
@@ -12,7 +12,9 @@ let initialBoard = [
   [false, false, true, false, false],
   [false, false, true, false, false],
   [false, false, true, false, false],
-  [false, false, false, false, false]
+  [false, false, false, false, false],
+  [true, false, true, false, false],
+  [true, false, true, true, true],
 ];
 
 export default function App() {
